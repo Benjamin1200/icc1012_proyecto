478bb33560cd96a4afb2589ca6ec54adc176ad05
Benja <bacorvalan@miuandes.cl>
2016-09-14 00:38:08 -0300

    Make the tweets_streaming file more 'efficient', space wise and working on tweets cursors (old tweets retrieval).

--- tweets_to_mongo.py
+++ tweets_to_mongo.py
@@ -1,6 +1,9 @@
 import json
 import pymongo
 
+# TODO: read from different files, 3 so far (the full tweet data one, the short tweet data and the cursor data.
+# TODO: verify that the tweet id is not already inside the mongoDB -> O(n)
+
 filename = "tweets_videogames.data"
 db = pymongo.MongoClient().project
 _file = open(filename, 'r')
