b1564991d2abcdd16f6ef22a024924b970cefb9c
Benja <bacorvalan@miuandes.cl>
2016-11-06 19:37:48 -0300

    Added the first version to count the languages by platform.

---
+++ Twitter/language_detect.py
@@ -0,0 +1,88 @@
+import sys
+import json
+reload(sys)
+sys.setdefaultencoding('utf8')
+try:
+    from nltk import wordpunct_tokenize
+    from nltk.corpus import stopwords
+except ImportError:
+    print '[!] You need to install nltk (http://nltk.org/index.html)'
+
+
+
+#----------------------------------------------------------------------
+def _calculate_languages_ratios(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return a dictionary that looks like {'french': 2, 'spanish': 4, 'english': 0}
+    
+    @param text: Text whose language want to be detected
+    @type text: str
+    
+    @return: Dictionary with languages and unique stopwords seen in analyzed text
+    @rtype: dict
+    """
+
+    languages_ratios = {}
+
+    '''
+    nltk.wordpunct_tokenize() splits all punctuations into separate tokens
+    
+    >>> wordpunct_tokenize("That's thirty minutes away. I'll be there in ten.")
+    ['That', "'", 's', 'thirty', 'minutes', 'away', '.', 'I', "'", 'll', 'be', 'there', 'in', 'ten', '.']
+    '''
+
+    tokens = wordpunct_tokenize(text)
+    words = [word.lower() for word in tokens]
+
+    # Compute per language included in nltk number of unique stopwords appearing in analyzed text
+    for language in stopwords.fileids():
+        stopwords_set = set(stopwords.words(language))
+        words_set = set(words)
+        common_elements = words_set.intersection(stopwords_set)
+
+        languages_ratios[language] = len(common_elements) # language "score"
+
+    return languages_ratios
+
+
+#----------------------------------------------------------------------
+def detect_language(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return the highest scored.
+    
+    It uses a stopwords based approach, counting how many unique stopwords
+    are seen in analyzed text.
+    
+    @param text: Text whose language want to be detected
+    @type text: str
+    
+    @return: Most scored language guessed
+    @rtype: str
+    """
+
+    ratios = _calculate_languages_ratios(text)
+
+    most_rated_language = max(ratios, key=ratios.get)
+
+    return most_rated_language
+
+
+def set_language_for_each_tweet(_file):
+    _data = open(_file, 'r')
+    open(_file[:-5] + "_language.data", 'w').close()
+    _output = open(_file[:-5] + "_language.data", 'a')
+    for line in _data.readlines():
+        record = json.loads(line)
+        language = detect_language(record['text'].encode('utf-8'))
+        try:
+            record.update({'language': language})
+            _output.write(json.dumps(record, sort_keys=True) + "\n")
+        except:
+            # Should not happen.
+            print "WTF!"
+    _data.close()
+    _output.close()
+    return _output.name
+
